083645c6a0a2637803e7c72d0d80fc8904b1f33a
George Whysall <george@hooddigital.com>
2019-04-06 18:07:27 +0100

    New fixedImage Tag Helper.

--- projects/Hood/TagHelpers/FixedImageTagHelper.cs
+++ projects/Hood/TagHelpers/FixedImageTagHelper.cs
@@ -7,10 +7,25 @@ namespace Hood.TagHelpers
     [HtmlTargetElement("fixedImage")]
     public class FixedImageTagHelper : TagHelper
     {
+        private const string AltAttrName = "alt";
+        [HtmlAttributeName(AltAttrName)]
+        public string Alt { get; set; }
 
         private const string SrcAttrName = "src";
         [HtmlAttributeName(SrcAttrName)]
         public string Src { get; set; }
+
+        private const string DefaultAttrName = "default";
+        [HtmlAttributeName(DefaultAttrName)]
+        public bool UseDefault { get; set; } = true;
+
+        private const string FallbackAttrName = "fallback";
+        [HtmlAttributeName(FallbackAttrName)]
+        public string Fallback { get; set; }
+
+        private const string ColourAttrName = "color";
+        [HtmlAttributeName(ColourAttrName)]
+        public string Colour { get; set; }
 
         private ISettingsRepository _settings;
 
@@ -21,36 +36,44 @@ namespace Hood.TagHelpers
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            string classValue;
-            if (output.Attributes.ContainsName("class"))
-            {
-                classValue = string.Format("{0} {1}", output.Attributes["class"].Value, "hood-image ");
-            }
-            else
-            {
-                classValue = "hood-image";
-            }
+            var mediaSettings = _settings.GetMediaSettings();
+
             output.TagName = "figure";
             output.TagMode = TagMode.StartTagAndEndTag;
-            var mediaSettings = _settings.GetMediaSettings();
-            string url = mediaSettings.NoImage.IsSet() ? mediaSettings.NoImage : "/lib/hood/images/no-image.jpg";
-            if (!string.IsNullOrEmpty(Src))
-            {
-                url = Src;
-            }
-            string styleValue;
-            if (output.Attributes.ContainsName("style"))
-            {
-                styleValue = string.Format("{0};{1}", output.Attributes["style"].Value, string.Format("background-image:url({0});", url)); 
-            }
-            else
-            {
-                styleValue = string.Format("background-image:url({0});", url);
-            }
-            output.Content.AppendHtml(output.Content.GetContent() + "<img src='" + url + "' alt='' />");
-            output.Attributes.SetAttribute("class", classValue);
-            output.Attributes.SetAttribute("style", styleValue);
-       }
-    }
 
+            string styleValue = "";
+
+            if (output.Attributes.ContainsName("style"))
+                styleValue += $"{output.Attributes["style"].Value};";
+
+            if (Colour.IsSet())
+                styleValue += $"background-color:{Colour};";
+
+            string url = "";
+
+            if (UseDefault)
+                if (mediaSettings.NoImage.IsSet())
+                    url = mediaSettings.NoImage;
+                else
+                    url = "/lib/hood/images/no-image.jpg";
+
+            if (Fallback.IsSet())
+                url = Fallback;
+
+            if (Src.IsSet())
+                url = Src;
+
+            if (url.IsSet())
+                styleValue += $"background-image:url({url});";
+
+            output.Attributes.SetAttribute("style", styleValue);
+
+            if (output.Attributes.ContainsName("class"))
+                output.Attributes.SetAttribute("class", $"{output.Attributes["class"].Value} hood-image");
+            else
+                output.Attributes.SetAttribute("class", $"hood-image");
+
+            output.PreContent.SetHtmlContent($"<img src='{url}' alt='{Alt}' />");
+        }
+    }
 }
