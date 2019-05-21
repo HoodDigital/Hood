88852d61994511bd5f5d984742a998b0c7eccc2f
George Whysall <george@hooddigital.com>
2019-05-21 15:12:41 +0100

    Added IsEurope flag to countries.

--- projects/Hood/Models/ComplexTypes/Country.cs
+++ projects/Hood/Models/ComplexTypes/Country.cs
@@ -1,4 +1,6 @@
-﻿namespace Hood.Models
+﻿using System.Collections.Generic;
+
+namespace Hood.Models
 {
     public class Country
     {
@@ -9,6 +11,69 @@
         public string FullName { get; set; }
         public string CurrencySymbol { get; set; }
         public string CurrencyName { get; set; }
+
+        public bool IsEurope
+        {
+            get
+            {
+                var europe = new List<string>()
+                {
+                    "AD",
+                    "AT",
+                    "BE",
+                    "BG",
+                    "HR",
+                    "CY",
+                    "CZ",
+                    "DK",
+                    "EE",
+                    "FO",
+                    "FI",
+                    "FR",
+                    "DE",
+                    "GI",
+                    "GR",
+                    "GL",
+                    "GG",
+                    "VA",
+                    "HU",
+                    "IS",
+                    "IE",
+                    "IM",
+                    "IL",
+                    "IT",
+                    "JE",
+                    "LV",
+                    "LI",
+                    "LT",
+                    "LU",
+                    "MK",
+                    "MT",
+                    "MC",
+                    "ME",
+                    "NL",
+                    "NO",
+                    "PL",
+                    "PT",
+                    "RO",
+                    "PM",
+                    "SM",
+                    "RS",
+                    "SK",
+                    "SI",
+                    "ES",
+                    "SJ",
+                    "SE",
+                    "CH",
+                    "TR",
+                    "GB",
+                    };
+                europe.ForEach(e => e = e.ToLower());
+                if (europe.Contains(Iso2.ToLower()))
+                    return true;
+                else return false;
+            }
+        }
 
         public Country(string name, string fullName, string iso2, string iso3, string numeric, string currencySymbol, string currencyName)
         {
