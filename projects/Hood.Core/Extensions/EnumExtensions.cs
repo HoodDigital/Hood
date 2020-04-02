80425cf4858acd53fbe12bc1c5b68000958262b1
George Whysall <george@hooddigital.com>
2020-04-02 14:21:16 +0100

    Added ToList() function to generic enums to iterate through.

--- projects/Hood.Core/Extensions/EnumExtensions.cs
+++ projects/Hood.Core/Extensions/EnumExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Hood.Extensions
 {
@@ -8,6 +10,10 @@ namespace Hood.Extensions
         {
             var output = (T)Enum.Parse(typeof(T), value, true);
             return output != null ? output : defaultValue;
+        }
+        public static IEnumerable<T> ToList<T>() where T : Enum
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>();
         }
     }
 }
