0942df897681810a595373140125a3ffc03822f9
George Whysall <george@hooddigital.com>
2021-06-11 14:42:01 +0100

    Removing old bg images, replacing with random unsplash. Tidying login settings.

--- projects/Hood.Core/Models/Settings/LoginAreaSettings.cs
+++ projects/Hood.Core/Models/Settings/LoginAreaSettings.cs
@@ -13,14 +13,6 @@ namespace Hood.Models
         {
             Logo = "https://cdn.jsdelivr.net/npm/hoodcms/images/hood-cms.png";
             Title = "Hood CMS";
-            BackgroundImages = "";
-            for (int i = 1; i <= 9; i++)
-            {
-                BackgroundImages += $"https://cdn.jsdelivr.net/npm/hoodcms/images/bg/{i}.jpg{Environment.NewLine}";
-            }
-            BackgroundImagesFadeSpeed = 1000;
-            BackgroundImagesChangeDuration = 3000;
-            RandomiseImages = true;
         }
 
         [Display(Name = "Area Logo", Description = "Change the logo for your login areas.")]
@@ -31,33 +23,6 @@ namespace Hood.Models
 
         [Display(Name = "Background Images", Description = "Change the background images for your login areas. Enter an image url on each line, these can be local or public URLs.")]
         public string BackgroundImages { get; set; }
-        [Display(Name = "Background Fade Speed", Description = "Change the speed for your image transitions.")]
-        public int BackgroundImagesFadeSpeed { get; set; } = 1000;
-        [Display(Name = "Background Change Duration", Description = "Change the time between image transitions.")]
-        public int BackgroundImagesChangeDuration { get; set; } = 3000;
-        [Display(Name = "Randomise Background Images?", Description = "Check this to shuffle the background images every time the pages load.")]
-        public bool RandomiseImages { get; set; } = true;
-
-        public IHtmlContent BackgroundImagesScript
-        {
-            get
-            {
-                IEnumerable<string> imageList = BackgroundImages.Trim().Split(Environment.NewLine).ToList();
-                string images = "[";
-                if (RandomiseImages)
-                {
-                    imageList = imageList.Shuffle();
-                }
-
-                foreach (string img in imageList)
-                {
-                    images += $"'{img.Trim()}',";
-                }
-                images.TrimEnd(',');
-                images += "]";
-                return new HtmlString($@"<script>$.backstretch({images},{{fade:{BackgroundImagesFadeSpeed},duration:{BackgroundImagesChangeDuration}}});</script>");
-            }
-        }
 
     }
 }
---
+++ projects/Hood.Development/src/webpack/app.production.js
@@ -0,0 +1,83 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const plugins = [];
+plugins.push(new MiniCssExtractPlugin({
+    filename: "css/[name].css"
+}));
+
+console.log('---------------------------------------------------------------------------')
+console.log('-                                                                         -')
+console.log('-                       Building Hood CMS - APP                           -')
+console.log('-                              PRODUCTION                                 -')
+console.log('-                                                                         -')
+console.log('---------------------------------------------------------------------------')
+
+module.exports = {
+
+    plugins,
+
+    entry: {
+        app: ['./src/ts/app.ts', './src/scss/app.scss']
+    },
+
+    mode: "production",
+
+    devtool: false,
+
+    module: {
+        rules: [
+
+            {
+                test: /\.tsx?$/,
+                use: [
+                    {
+                        loader: 'ts-loader'
+                    }
+                ],
+                exclude: [/node_modules/, /wwwroot/],
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ],
+    },
+
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.scss'],
+    },
+
+    externals: {
+        jquery: 'jQuery',
+        bootstrap: 'bootstrap'
+    },
+
+    output: {
+        filename: 'js/[name].js',
+        path: path.resolve("./wwwroot/dist")
+    },
+
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin()
+        ]
+    },
+
+};
