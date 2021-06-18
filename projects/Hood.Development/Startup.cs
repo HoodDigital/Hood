ae2f7ae5e55fcfaf82b147eac65c52eb80635e43
George Whysall <george@hooddigital.com>
2021-06-18 16:52:23 +0100

    Removed component from main app.

--- projects/Hood.Development/Startup.cs
+++ projects/Hood.Development/Startup.cs
@@ -18,15 +18,16 @@ namespace Hood.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureHood<ApplicationDbContext>(config);
             services.AddHood(config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseHoodDefaults(env, config);
+            app.UseHoodDefaultRoutes(config);
             app.UseHood(env, config);
         }
-
-
     }
 }
--- projects/Hood.Web/Startup.cs
+++ projects/Hood.Web/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Hood.Startup;
+﻿using Hood.Models;
+using Hood.Startup;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -18,15 +19,16 @@ namespace Hood.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureHood<ApplicationDbContext>(config);
             services.AddHood(config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseHoodDefaults(env, config);
+            app.UseHoodDefaultRoutes(config);
             app.UseHood(env, config);
         }
-
-
     }
 }
