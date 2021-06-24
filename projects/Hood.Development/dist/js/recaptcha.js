eb4c6c71567e5705558e41931ee79ae278278bcf
George Whysall <george@hooddigital.com>
2021-06-24 13:01:07 +0100

    Removing compiled files from version control.

--- projects/Hood.Development/dist/js/recaptcha.js
+++
@@ -1,8 +0,0 @@
-var grecaptcha;
-function hood__getReCaptcha(key, recaptchaId, action) {
-    grecaptcha.ready(function () {
-        grecaptcha.execute(key, { 'action': action }).then(function (token) {
-            document.getElementById(recaptchaId).value = token;
-        });
-    });
-}
