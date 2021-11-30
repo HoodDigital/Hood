94d3f29012ffc553bfada4ddff475c322df746a2
George Whysall <george@hooddigital.com>
2021-11-30 16:35:35 +0000

    Added GetFromCache<T> extension to IHoodCache.

---
+++ projects/Hood.Core/Extensions/IHoodCacheExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Hood.Caching;
+using System;
+
+namespace Hood.Extensions
+{
+    public static class IHoodCacheExtensions
+    {
+        public static T GetFromCache<T>(this IHoodCache cache, string key, Func<T> loadAction)
+        {
+            if (cache.TryGetValue(key, out T cachedObject))
+            {
+                return cachedObject;
+            }
+            cachedObject = loadAction.Invoke();
+            if (cachedObject != null)
+            {
+                cache.Add(key, cachedObject);
+            }
+            return cachedObject;
+        }
+    }
+}
