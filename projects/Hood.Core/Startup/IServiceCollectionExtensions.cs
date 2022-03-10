c18670e1148e35789d5a54a47762ddabd238e9c4
George Whysall <george@hooddigital.com>
2022-03-10 18:57:30 +0000

    Update IServiceCollectionExtensions.cs

--- projects/Hood.Core/Startup/IServiceCollectionExtensions.cs
+++ projects/Hood.Core/Startup/IServiceCollectionExtensions.cs
@@ -322,6 +322,7 @@ namespace Hood.Startup
 
             options.Cookie.Name = $"{cookieName}_auth";
             options.Cookie.HttpOnly = true;
+            options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
             options.Cookie.Domain = config["Identity:Cookies:Domain"].IsSet() ? config["Identity:Cookies:Domain"] : null;
 
             options.AccessDeniedPath = config["Identity:AccessDeniedPath"].IsSet() ? config["Identity:AccessDeniedPath"] : "/account/access-denied";
