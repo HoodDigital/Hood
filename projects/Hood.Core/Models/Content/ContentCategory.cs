16e8ff7f4cbeacd08d225918d3da95f1624b706e
George Whysall <george@hooddigital.com>
2022-03-10 18:57:53 +0000

    Unified Json serialization libraries.

--- projects/Hood.Core/Models/Content/ContentCategory.cs
+++ projects/Hood.Core/Models/Content/ContentCategory.cs
@@ -1,9 +1,9 @@
 ﻿using Hood.Entities;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Runtime.Serialization;
-using System.Text.Json.Serialization;
 
 namespace Hood.Models
 {
