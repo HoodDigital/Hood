013a474b5c006a39dad73e5f23a9f071c05eb45e
George Whysall <george@hooddigital.com>
2022-09-16 11:20:00 +0100

    Added migration refs to sql.

--- sql/6.0/migrate.sql
+++ sql/6.0/migrate.sql
@@ -7,6 +7,16 @@ BEGIN
 END;
 GO
 
+IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
 INSERT INTO [__HoodMigrationHistory] ([MigrationId])
 VALUES (N'Hood_ScriptMigrations_v6.0');
 GO
