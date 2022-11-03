87e3240234be53d36be8a4112d1206ecd97f7f82
George Whysall <JorjeRedemption@users.noreply.github.com>
2022-11-03 10:19:46 +0000

    Update AccountRepository.cs

--- projects/Hood.Core/Services/AccountRepository/AccountRepository.cs
+++ projects/Hood.Core/Services/AccountRepository/AccountRepository.cs
@@ -115,12 +115,12 @@ namespace Hood.Services
                 .Include(u => u.Addresses)
                 .SingleOrDefaultAsync(u => u.Id == userId);
 
-            if (user.Email == Engine.Configuration.SuperAdminEmail)
+            if (user.UserName == Engine.Configuration.SuperAdminEmail)
             {
                 throw new Exception("You cannot delete the site owner account, the owner is set via an environment variable and cannot be changed from the admin area.");
             }
 
-            ApplicationUser siteOwner = await _db.Users.AsNoTracking().SingleOrDefaultAsync(u => u.Email == Engine.Configuration.SuperAdminEmail);
+            ApplicationUser siteOwner = await _db.Users.AsNoTracking().SingleOrDefaultAsync(u => u.UserName == Engine.Configuration.SuperAdminEmail);
             if (siteOwner == null)
             {
                 throw new Exception("Could not load the owner account, check your settings, the owner is set via an environment variable and cannot be changed from the admin area.");
